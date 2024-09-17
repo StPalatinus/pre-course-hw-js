4190902096d035f659c6ba73bef19a45beaca79f
StPalatinus <PalaCisco@gmail.com>
2024-09-17 21:42:17 +0300

    HW 01/05 tr-2

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,7 +1,7 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
 switch (true) {
-    case rule.length = 25:
+    case rule.length == 25:
         console.log("50 на 50");
         break;
     case rule.length > 25:
