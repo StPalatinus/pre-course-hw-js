f01fa85b5c306d26dc9247f67ed9e45ead81f18c
StPalatinus <PalaCisco@gmail.com>
2024-09-22 14:44:06 +0300

    HW 02/05 tr-1

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport2 = Object.assign({}, passport);
+
+passport2.name = "Ivan";
+
+console.log(passport.name);
+console.log(passport2.name);
+// console.log(passport.name === passport2.name);
